cd2e2a7845916aba9c4571c59b05a7707cba4cd6
Witer33 <34513257+witer33@users.noreply.github.com>
2025-06-11 13:33:51 +0200

    new gemini 2.5 pro

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import threading
 
 
 models = [
+    "gemini-2.5-pro-preview-06-05",
     "gemini-2.5-pro-preview-05-06",
     "gemini-2.5-flash-preview-04-17",
     "gemini-2.5-flash-preview-05-20",
